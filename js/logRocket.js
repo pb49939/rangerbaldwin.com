346c18b76b10873cf8e7767ce23f23198a30a5ed
Patrick Baldwin <patrickbaldwin@quickenloans.com>
2021-03-30 15:14:14 -0400

    Moving log rocket init to js

--- js/logRocket.js
+++ js/logRocket.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    LogRocket.init('1fena1/ranger-baldwin-poc');
     LogRocket.identify('123456', {
         name: 'Patrick Baldwin',
         email: 'pbaldwi3@gmail.com',
